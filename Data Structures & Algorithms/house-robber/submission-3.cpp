d6801ac2a76e5759fe1f9fcff7dce5f44e96f139
sumantopal07 <sumantopal07@gmail.com>
2026-08-06 20:59:25 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int rob(vector<int>& ar) {
+        int n=ar.size();
+        if(n==0)
+            return 0;
+        if(n==1)
+            return ar[0];
+        int max_prev_prev = ar[0];
+        int max_prev=max(ar[1],ar[0]);
+        int ans = max(max_prev_prev,max_prev );
+        for(int i=2;i<n;i++) {
+            int temp=max(max_prev, ar[i]+max_prev_prev);
+            max_prev_prev = max_prev;
+            max_prev = temp;
+            ans = max(max_prev , max_prev_prev);
+        }
+            ans = max(max_prev , max_prev_prev);
+
+        return ans;
+
+    }
+};
+// class Solution {
+// public:
+//     int minCostClimbingStairs(vector<int>& ar) {
+
+        
+//     }
+// };
